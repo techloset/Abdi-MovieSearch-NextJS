d7b9c388705a4d28bef2357ad1450df477bfdbd2
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-12 01:49:45 +0500

    done

--- app/(component)/MovieBox.jsx
+++ app/(component)/MovieBox.jsx
@@ -1,7 +1,8 @@
 import { Modal, Button } from "react-bootstrap";
 import React, { useState } from "react";
-const API_IMG = "https://image.tmdb.org/t/p/w500/";
+// import noImage from "../../public";
 
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
 const MovieBox = ({
   title,
   poster_path,
@@ -11,13 +12,18 @@ const MovieBox = ({
   overview,
 }) => {
   const [show, setShow] = useState(false);
+  const img = API_IMG + poster_path;
 
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
   return (
     <div className="card text-center bg-warning mb-3 col-md-4 col-lg-2  ">
-      <img className="card-img-top" src={API_IMG + poster_path} />
+      <img
+        className="card-img-top"
+        src={!poster_path ? "/assets/noImage.jpg" : img}
+        alt={title}
+      />
       <div className="card-body ">
         <h4>IMDb Rating: {vote_average}</h4>
         <h5>Release Date: {release_date}</h5>
@@ -30,9 +36,10 @@ const MovieBox = ({
           </Modal.Header>
           <Modal.Body>
             <img
-              className="card-img-top"
+              className="card-img-top img-fluid"
               style={{ width: "100%" }}
               src={API_IMG + backdrop_path}
+              alt={title}
             />
             <h3>{title}</h3>
             <h4>IMDb: {vote_average}</h4>
