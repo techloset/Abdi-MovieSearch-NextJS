fcb3cf52fddeb4a4b2c82041e7be063b894564c2
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-21 00:48:16 +0500

    ready for peer review

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
-
 export const metadata = {
   title: "Movies Database ",
   description: "Movies Database by Abdi",
